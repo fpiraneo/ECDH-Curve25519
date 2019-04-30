52ca7a484093575f223c9861aa223b3f525b2061
Francesco Piraneo G <francesco@piraneo-canepa.ch>
2019-04-30 13:11:07 +0200

    Initial Commit

---
+++ ECDH-Curve25519/ECDH_Curve25519.h
@@ -0,0 +1,19 @@
+//
+//  ECDH_Curve25519.h
+//  ECDH-Curve25519
+//
+//  Created by Francesco Piraneo G. on 30.04.19.
+//  Copyright © 2019 Francesco Piraneo G. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ECDH_Curve25519.
+FOUNDATION_EXPORT double ECDH_Curve25519VersionNumber;
+
+//! Project version string for ECDH_Curve25519.
+FOUNDATION_EXPORT const unsigned char ECDH_Curve25519VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ECDH_Curve25519/PublicHeader.h>
+
+
